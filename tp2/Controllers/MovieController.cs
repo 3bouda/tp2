b950e8b6af4a93c1c813954f48ede7e2e597eb92
3bouda <abdelaliiheb@gmail.com>
2024-07-02 02:28:05 +0100

    Add project files.

---
+++ tp2/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tp2.Models;
+
+namespace tp2.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Index
+        public ActionResult Index()
+        {
+            Movie movie = new Movie() { Name = "movie 1" };
+            List<Movie> movies = new List<Movie>()
+                {
+                new Movie{Name="movie 2"},
+                new Movie{Name="movie 3"},
+                };
+            return View(movies);
+        }
+        public ActionResult Edit(int Id) {
+            return Content("Id" + Id);
+        }
+        public ActionResult byRelease(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ tp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tp2.Startup))]
+namespace tp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
